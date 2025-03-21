5ed4f1209179d362a76dcb141b31ba0252c308a5
rogerio-alvesf <rogeriobusiness@protonmail.com>
2025-03-21 18:26:14 -0300

    Add: Search filter

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -3,26 +3,65 @@ import { MatTableModule } from '@angular/material/table';
 import { Course } from './course';
 import { Star } from '../star/star.component';
 import { CourseService } from './courses.services';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs/internal/Observable';
+import { map, startWith } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
+import { ReplacePipe } from '../pipe/replace.pipe';
 
 @Component({
-  imports: [MatTableModule, Star],
-  selector: 'app-course-list',
+  imports: [
+    ReplacePipe,
+    CommonModule,
+    MatTableModule,
+    Star,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
   templateUrl: './course-list.component.html',
-  styleUrl: './course-list.component.css',
+  styleUrls: ['./course-list.component.css'],
 })
 export class CourseListComponent implements OnInit {
   courses: Course[] = [];
-
   dataSource = this.courses;
-  displayedColumns: string[] = ['imageUrl', 'name', 'price', 'code', 'duration', 'rating', 'releaseDate'];
+  displayedColumns: string[] = [
+    'imageUrl',
+    'name',
+    'price',
+    'code',
+    'duration',
+    'rating',
+    'releaseDate',
+  ];
 
-  constructor(private courseService: CourseService)
-  {
+  myControl = new FormControl('');
 
-  }
+  filteredOptions: Observable<Course[]> = new Observable();
+
+  constructor(private courseService: CourseService) {}
 
   ngOnInit(): void {
     this.courses = this.courseService.retrieveAll();
     this.dataSource = [...this.courses];
+
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || ''))
+    );
+  }
+
+  private _filter(value: string): Course[] {
+    const filterValue = value.toLowerCase();
+    return this.courses.filter((course) =>
+      course.name.toLowerCase().includes(filterValue)
+    );
   }
 }
